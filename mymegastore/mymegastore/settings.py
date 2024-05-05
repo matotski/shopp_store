2e88fe562ac472ffed08458af3e39fe8303306e9
ddfxfxfc <fetodfkjghjdhgjjdhsjejldahjckhjkcahxcjhxjk>
2024-05-05 17:26:54 +1000

    d29

--- mymegastore/mymegastore/settings.py
+++ mymegastore/mymegastore/settings.py
@@ -133,3 +133,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = 'users.user'
 
 LOGIN_URL = '/users/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'jamalsigma11@gmail.com'
+EMAIL_HOST_PASSWORD = 'dpwl chsu hwpp beam'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
--- mymegastore/mymegastore/urls.py
+++ mymegastore/mymegastore/urls.py
@@ -19,12 +19,18 @@ from django.urls import path, include
 from products.views import index, products
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('', index, name = 'index' ),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
+    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name = 'admin_password_reset'),
+    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
+    path('admin/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
+    path('admin/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'),
+    path('admin/', admin.site.urls),
 
 ]
 
